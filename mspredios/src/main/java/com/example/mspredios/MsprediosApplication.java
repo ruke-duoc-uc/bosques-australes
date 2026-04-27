fd1cb542b00fd97ff5b833584e6ed35eff9f96c7
ruke-duoc-uc <cri.loncon@duocuc.cl>
2026-04-27 12:42:02 -0400

    ms-predios Cristobal

---
+++ mspredios/src/main/java/com/example/mspredios/MsprediosApplication.java
@@ -0,0 +1,13 @@
+package com.example.mspredios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprediosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsprediosApplication.class, args);
+	}
+
+}
