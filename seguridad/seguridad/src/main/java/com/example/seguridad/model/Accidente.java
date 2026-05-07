ce4244410a73cad1ebc9034f9ccc313a05020871
Alvarooyar <alv.oyarzun@duocuc.cl>
2026-05-07 10:56:30 -0400

    feat: agrege el microservicio de seguridad, no esta completo todavia

---
+++ seguridad/seguridad/src/main/java/com/example/seguridad/model/Accidente.java
@@ -0,0 +1,9 @@
+package com.example.seguridad.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "accidentes")
+public class Accidente {
+}
---
+++ seguridad/seguridad/src/main/java/com/example/seguridad/model/TipoAccidente.java
@@ -0,0 +1,6 @@
+package com.example.seguridad.model;
+
+public enum TipoAccidente {
+    ACCIDENTE,
+    CASI_ACCIDENTE
+}
