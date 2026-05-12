fbecd0ee86d149674285f38ce1d87274fb4a52fa
isiayala <isi.ayala@duocuc.cl>
2026-05-12 18:50:44 -0400

    refactor: creo el microservicio completo

---
+++ src/main/java/com/example/mstrabajadores/MstrabajadoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.mstrabajadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstrabajadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstrabajadoresApplication.class, args);
+	}
+
+}
