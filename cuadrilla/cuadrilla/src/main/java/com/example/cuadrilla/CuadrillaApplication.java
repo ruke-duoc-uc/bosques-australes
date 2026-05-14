6c14ec330700964c23f153e91393f286e2545c96
Alvarooyar <alv.oyarzun@duocuc.cl>
2026-05-14 16:17:12 -0400

    refactor: mejore alguna parte del codigo

---
+++ cuadrilla/cuadrilla/src/main/java/com/example/cuadrilla/CuadrillaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cuadrilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuadrillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuadrillaApplication.class, args);
+	}
+
+}
