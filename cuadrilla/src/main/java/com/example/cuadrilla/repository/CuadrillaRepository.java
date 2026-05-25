02dbad726f408de5a329a0ca0c99c00d9d24935d
ruke-duoc-uc <cri.loncon@duocuc.cl>
2026-05-24 22:00:24 -0400

    docs: Contexto de proyecto y eliminacion de texto innecesario

--- cuadrilla/src/main/java/com/example/cuadrilla/repository/CuadrillaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.cuadrilla.repository;
-
-import com.example.cuadrilla.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CuadrillaRepository extends JpaRepository<Cuadrilla, Long>{
-}
