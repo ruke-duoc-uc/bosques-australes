bc0a1d2492f54f46394f5d7e1c89aa8b34220e76
Alvarooyar <alv.oyarzun@duocuc.cl>
2026-06-02 09:29:11 -0400

    refactor: incopore el Dto en Cliente

---
+++ mscliente/src/main/java/com/example/cliente/Dto/ClienetResponseDto.java
@@ -0,0 +1,120 @@
+package com.example.cliente.Dto;
+
+import com.example.cliente.model.TipoCliente;
+import java.util.List;
+import java.util.Map;
+
+public class ClienetResponseDto {
+    private long id;
+    private String nombre;
+    private String rut;
+    private String razonSocial;
+    private String direccion;
+    private String comuna;
+    private String ciudad;
+    private String telefono;
+    private String email;
+    private TipoCliente tipoCliente;
+    private Boolean estado;
+
+    private List<Map<String, Object>> contratosAsociados;
+
+    //getters and setters
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
+    public void setComuna(String comuna) {
+        this.comuna = comuna;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TipoCliente getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(TipoCliente tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public List<Map<String, Object>> getContratosAsociados() {
+        return contratosAsociados;
+    }
+
+    public void setContratosAsociados(List<Map<String, Object>> contratosAsociados) {
+        this.contratosAsociados = contratosAsociados;
+    }
+}
