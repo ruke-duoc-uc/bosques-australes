080a6752440eec45ff898128e9991be9b8f920ab
isiayala <isi.ayala@duocuc.cl>
2026-07-05 17:37:47 -0400

    docs: agrego comentarios completos a trabajadores

--- mstrabajadores/src/main/java/com/example/mstrabajadores/model/Cargo.java
+++ mstrabajadores/src/main/java/com/example/mstrabajadores/model/Cargo.java
@@ -1,5 +1,9 @@
 package com.example.mstrabajadores.model;
 
+/**
+ * Enum que representa los distintos cargos que puede ocupar
+ * un trabajador dentro de la empresa.
+ */
 public enum Cargo {
     DESPACHADOR,
     PODADOR,
